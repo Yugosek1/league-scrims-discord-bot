987c91317de706548bbb4e6684d0539639676a47
Yugosek1 <21911203ys@tama.ac.jp>
2021-07-15 03:14:17 +0900

    develop

--- dbot_forscrims.py
+++ dbot_forscrims.py
@@ -57,6 +57,7 @@ async def on_message(message):
 
 async def post_add(message):
    post_message = re.findall(r'^!post (.+),(\d{4}-\d{2}-\d{2} \d{2}:\d{2}),(\d),(.+),(.+)$',message.content)
+   print(post_message)
    if post_message:
       cur.execute('insert into database(user_id, user_name, created_datetime, teamname, date_and_time, tier_average , matches, comments)values(%s, %s, %s, %s, %s, %s, %s, %s) RETURNING id',
                   [message.author.id, message.author.name, message.created_at, post_message[0], post_message[1], post_message[2], post_message[3], post_message[4]])
